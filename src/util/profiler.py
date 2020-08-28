5eacb2ff0984cda4dd50a2e2422863198114bfcb
Alexander <gogogox215@gmail.com>
2020-08-28 14:32:48 +0300

    Added profiler

---
+++ src/util/profiler.py
@@ -0,0 +1,12 @@
+import time
+import functools
+
+def profile(f):
+    @functools.wraps(f)
+    def wrap(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+        print (f'func:{f.__name__} args:[{args}, {kw}] took: {te-ts:2.4f} sec')
+        return result
+    return wrap
