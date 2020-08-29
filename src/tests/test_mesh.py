50ee9227bfbb7ae3f6c692389012cd6a23a8e6d9
Alexander <gogogox215@gmail.com>
2020-08-29 03:46:27 +0300

    optimization to for loop

--- src/tests/test_mesh.py
+++ src/tests/test_mesh.py
@@ -1,15 +1,15 @@
 import glob
-import unittest
 import os
-from src.util.timing import profile
+import unittest
 
 from src.geometry.numpy.mesh import *
+from src.util.timing import profile
 
 
 class TestMesh(unittest.TestCase):
     def setUp(self):
         self.off_files = glob.glob('data/example_off_files/*.off')
-        self.mesh = Mesh('data/example_off_files/phands.off')
+        self.mesh = Mesh('data/example_off_files/cat.off')
 
     @profile
     def test_get_vertex_valence(self):
@@ -42,12 +42,11 @@ class TestMesh(unittest.TestCase):
         for file in self.off_files:
             data = read_off(file)
             temp_file = 'src/tests/temp/tempFile'
-            # if not os.path.exists(temp_file):
-            #     os.mknod(temp_file)
             write_off(data, temp_file)
             mesh_new = read_off(temp_file)
             self.assertTrue(np.array_equal(data[0], mesh_new[0]) and np.array_equal(data[1], mesh_new[1]))
             os.remove(temp_file)
+
 
     def test_visualization(self):
         """"
@@ -64,52 +63,51 @@ class TestMesh(unittest.TestCase):
          Args
                 path:  path for the .off file
         """
-        size_of_black_sphere = 1/3
-        # hyper parameter that decide the rate between the longest distance to the sphere to the size of the sphere
+        for file in self.off_files:
+            mesh = Mesh(file)
+            # hyper parameter that decide the rate between the longest distance to the sphere to the size of the sphere
+            size_of_black_sphere = 0.05
 
-        plotter = pv.Plotter(shape=(3, 3))
+            plotter = pv.Plotter(shape=(3, 3))
+            mesh.plot_vertices(f=lambda a: 0, index_row=0, index_col=0, show=False, plotter=plotter, title="vertices")
+            mesh.plot_faces(f=lambda a: 0, index_row=0, index_col=1, show=False, plotter=plotter, title="faces")
+            mesh.plot_wireframe(index_row=0, index_col=2, show=False, plotter=plotter, title="wireframe")
 
-        self.mesh.plot_vertices(f=lambda a: 0, index_row=0, index_col=0, show=False, plotter=plotter, title="vertices")
-        self.mesh.plot_faces(f=lambda a: 0, index_row=0, index_col=1, show=False, plotter=plotter, title="faces")
-        self.mesh.plot_wireframe(index_row=0, index_col=2, show=False, plotter=plotter, title="wireframe")
+            faces_barycenter = mesh.get_face_barycenters()
+            normals = mesh.get_face_normals()
 
-        faces_barycenter = self.mesh.get_face_barycenters()
-        normals = self.mesh.get_face_normals()
+            plotter.subplot(1, 0)
+            plotter.add_text("un-normalized normals", position='upper_edge', font_size=10)
+            plotter.add_arrows(faces_barycenter, normals)
 
-        plotter.subplot(1, 0)
-        plotter.add_text("un-normalized normals", position='upper_edge', font_size=10)
-        plotter.add_arrows(faces_barycenter, normals)
+            plotter.subplot(1, 1)
+            plotter.add_text("normalized normals", position='upper_edge', font_size=10)
+            normals = mesh.get_face_normals(norm=True)
+            plotter.add_arrows(faces_barycenter, normals)
 
-        plotter.subplot(1, 1)
-        plotter.add_text("normalized normals", position='upper_edge', font_size=10)
-        normals = self.mesh.get_face_normals(norm=True)
-        plotter.add_arrows(faces_barycenter, normals)
+            #  creating (x,y,z) -> id dict
+            table = {}
+            mean = np.array([0, 0, 0],dtype=np.float64)
 
-        #  creating (x,y,z) -> id dict
-        table = {}
-        mean = (0, 0, 0)
+            for i, cord in enumerate(mesh.vertices):
+                table[cord.tobytes()] = i
 
-        i = 0
-        for cord in self.mesh.vertices:
-            table[np.array_str(cord)] = i
-            i += 1
-            mean += cord
+            mean =mesh.vertices.mean()
 
-        mean = mean / (i + 1)
+            mesh.plot_vertices(f=lambda a: mesh.get_vertex_valence(table[a.tobytes()]),
+                               index_row=1, index_col=2, show=False, plotter=plotter,
+                               title='valance figure')
 
-        self.mesh.plot_vertices(f=lambda a: self.mesh.get_vertex_valence(table[np.array_str(a)]),
-                                index_row=1, index_col=2, show=False, plotter=plotter,
-                                title='valance figure')
 
-        plotter.subplot(2, 0)
-        max_dist = np.apply_along_axis(lambda a: np.linalg.norm(a - mean), 1, self.mesh.vertices).max()
-        self.mesh.plot_vertices(f=lambda a: np.linalg.norm(a - mean),
-                                #  L2 distance between the mean and the point
-                                index_row=2, index_col=0, show=False, plotter=plotter,
-                                title="distance from mean")
-        plotter.add_mesh(mesh=pv.Sphere(center=mean, radius=size_of_black_sphere * max_dist), color='black')
-
-        plotter.show()
+            plotter.subplot(2, 0)
+            max_dist = np.apply_along_axis(lambda a: np.linalg.norm(a - mean), 1, mesh.vertices).max()
+            mesh.plot_vertices(f=lambda a: np.linalg.norm(a - mean),
+                               #  L2 distance between the mean and the point
+                               index_row=2, index_col=0, show=False, plotter=plotter,
+                               title="distance from mean")
+            plotter.add_mesh(mesh=pv.Sphere(center=mean, radius=size_of_black_sphere * max_dist), color='black')
+            plotter.show(title=file)
+            plotter.close()
 
 
 if __name__ == '__main__':
