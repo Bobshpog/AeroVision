a26dae1095abfd8d19b1d8d9084c2dfb5d4deff7
Alexander <gogogox215@gmail.com>
2020-09-14 07:43:15 +0300

    updated depth  matrix to represent absolute distance

--- src/data/database.py
+++ src/data/database.py
@@ -7,22 +7,46 @@ import numpy as np
 import pyvista as pv
 
 from geometry.numpy.mesh import Mesh
-
+from geometry.numpy.wing_models import FemWing
 
 def process_csv(file):
-    pass #TODO implement
+    pass  # TODO implement
 
 
 class Config:
-    mesh_path = 'data/wing_off_files/combined_wing.off'
+    mesh_hull_path = 'data/wing_off_files/finished_fem_without_tip.off'
+    mesh_tip_path = 'data/wing_off_files/fem_tip.off'
     im_width = 640
     im_height = 480
     resolution = [im_width, im_height]
-    num_scales = 6
-    num_vertices_input = -1  # TODO
+    num_scales = 11  # number of modal scales
+    num_vertices_input = 9649
     compression = 'gzip'
     ir_count = 28
-    cameras = []  # TODO, insert camera angles
+    # cameras in pyvista format
+    cameras = [[(0.047, -0.053320266561896174, 0.026735639600027315),
+                (0.05, 0.3, 0.02),
+                (0, 0, 1)],
+
+               [(0.04581499400545182, -0.04477050005202985, -0.028567355483893577),
+                (0.05, 0.3, 0.02),
+                (0.001212842435223535, 0.13947688005070646, -1)],
+
+               [(0.11460619078012961, -0.04553696541254279, 0.038810512823530784),
+                (0.05, 0.3, 0.02),
+                (0, 0.16643488101070833, 1)],
+
+               [(0.11460619078012961, -0.04553696541254279, -0.038810512823530784),
+                (0.05, 0.3, 0.02),
+                (0, 0.16643488101070833, -1)],
+
+               [(-0.019770941905445285, -0.06082136750543311, 0.038694507832388224),
+                (0.05, 0.3, 0.02),
+                (0.041, 0.0438, 1)],
+
+               [(-0.019770941905445285, -0.06082136750543311, -0.038694507832388224),
+                (0.05, 0.3, 0.02),
+                (0.041, 0.0438, -1)]]
     texture = 'data/textures/checkers2.png'
 
 
@@ -30,19 +54,21 @@ class DatabaseBuilder:
 
     def __init__(self, data_folder):
         self.root_data_folder = Path(data_folder)
-        self.synthetic_data_folder = Path(self.root_data_folder / "synthetic data")
+        self.synthetic_data_folder = Path(self.root_data_folder / "synthetic_data")
         self.db_folder = Path(data_folder / 'databases')
         if not self.db_folder.exists():
             os.mkdir(self.db_folder)
 
     def __call__(self, output_path=None):
         if output_path is None:
-            output_path = self.db_folder / f"{str(datetime.now())}.hdf5"  # TODO fix path with relevant metadata
+            texture_name=Config.texture.split('/')[-1]
+            mesh_name=Config.mesh_path.split('/')[-1]
+            output_path = self.db_folder / f"{str(datetime.now())}||{Config.cameras}||{mesh_name}||{texture_name}.hdf5"  # TODO fix path with relevant metadata
         video_dirs = next(os.walk(self.synthetic_data_folder))[1]
         size = len(video_dirs)
         with h5py.File(output_path, 'w') as hf:
             dset_images = hf.create_dataset('images',
-                                            shape=(size, Config.num_angles, Config.im_width, Config.im_height, 4),
+                                            shape=(size, len(Config.cameras), Config.im_width, Config.im_height, 4),
                                             compression=Config.compression,
                                             dtype=np.float)  # TODO make sure image is in float format
             dset_scales = hf.create_dataset('scales', shape=(size, Config.num_scales), compression=Config.compression,
@@ -52,7 +78,8 @@ class DatabaseBuilder:
             dset_ir = hf.create_dataset('ir', shape=(size, Config.ir_count, 3), compression=Config.compression,
                                         dtype=np.float)
             dset_movements = hf.create_dataset('movements', shape=(size, Config.num_vertices_input, 3), dtype=np.float)
-            mesh = Mesh(Config.mesh_path)
+            mesh_wing_hull=Mesh(Config.mesh_hull_path)
+            mesh_wing_tip=Mesh(Config.mesh_tip_path)
             plotter = pv.Plotter()
             for folder_name in video_dirs:
                 folder_files = next(os.walk(folder_name))[2]
@@ -61,9 +88,9 @@ class DatabaseBuilder:
                     # extract a single movement from csv
                     movement, scales = process_csv(csvfile)
                     dset_movements[idx] = movement
-                    movement_reconstructed = mesh.get_movement_reconstructed(movement)
                     dset_scales[idx] = scales
-                    dset_ir[idx] = mesh.get_ir_coords(movement_reconstructed)
+
+                    dset_ir[idx] = FemWing.get_ir_coords()
                     dset_video_names[idx] = folder_name
                     for j, camera in enumerate(Config.cameras):
                         dset_images[idx, j] = mesh.get_photo(movement_reconstructed, resolution=Config.resolution,
--- src/data/sensors_unifier.py
+++ src/data/sensors_unifier.py
@@ -16,3 +16,29 @@
 # manifest_vid_<datetime>:
 # |IR|---|IR||CAM0(links to files)|---|CAM99|IMU0|---|IMU99|
 # We synchronize the diffrenet frequencies by saving the highest one and replicaing previous entries in between slow device updates
+
+import csv
+from pathlib import Path
+
+
+class Config:
+    num_points=28
+    num_cams=1
+    num_imus=1
+    time_format = "%Y_%m_%d_%H_%M_%S_%f"  # <Year>_<Month>_<Day>_<Hour>_<Minute>_<Sec>_<MilliSec>
+
+# input folder must include: an input file for each device:
+# cam<id>_<timestamp>.mp4
+# imu<id>_<timestamp>.txt
+# ir.csv
+def unify(path_to_input_folder, path_to_data_folder, cam_num, imu_num=1):
+    input_folder=Path(path_to_input_folder)
+    output_folder=Path(path_to_data_folder)
+    with open(input_folder /"ir.csv","r") as ir_csv, open(output_folder / "manifest.csv","w") as manifest_csv:
+        pass
+
+with open('data/data_samples/IR Lab conditions.csv') as csvfile:
+    content=csv.DictReader(csvfile)
+    for i in content:
+        print (i)
+print(df)
