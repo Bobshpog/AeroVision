700ae6ec23c949c108b1ef532b784d73ab97db89
Alexander <gogogox215@gmail.com>
2020-09-17 16:56:24 +0300

    Added comments

--- src/data/data_generators/data_gen.py
+++ src/data/data_generators/data_gen.py
@@ -1,21 +1,34 @@
 from abc import ABC, abstractmethod
+from functools import lru_cache
 
 import h5py
 import numpy as np
 
 
 class DataGenerator(ABC):
-
-    @abstractmethod
+    """
+    Abstract Data Generator base class
+    """
+    @lru_cache(1)
     def __len__(self) -> int:
+        """
+        Returns:
+            Number of values in iterator
+        """
+        return len(list(self))
+
+    @abstractmethod
+    def save_metadata(self, hdf5: h5py.File, group_name: str) -> None:
+        """
+        Saves metadata about the database
+        Args:
+            hdf5: h5py object
+            group_name:  name of the group
+        """
         pass
 
     @abstractmethod
-    def save_metadata(self, hdf5: h5py.File, group_name:str) -> None:
-        pass
-
-    @abstractmethod
-    def __iter__(self) -> (str,np.array, np.array):
+    def __iter__(self) -> (str, np.array, np.array):
         """
         Iterator that generates data
         Returns:
