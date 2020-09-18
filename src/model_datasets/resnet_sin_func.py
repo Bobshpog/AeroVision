e2917b09b027d4b885457f5947236cf5f78ca5f1
Alexander <gogogox215@gmail.com>
2020-09-18 22:48:33 +0300

    added option for transforms on dataset

--- src/model_datasets/resnet_sin_func.py
+++ src/model_datasets/resnet_sin_func.py
@@ -3,15 +3,27 @@ from torch.utils.data import Dataset
 
 
 class SinFunctionDataset(Dataset):
-    def __init__(self, hdf5_path ):
+    def __init__(self, hdf5_path, transforms=None):
+        """
+        Initialization
+        Args:
+            hdf5_path: path to hdf5 database
+            transforms: list of transforms to perform on the images
+        """
         super.__init__()
-        self.hf=h5py.File(hdf5_path, 'r')
+        self.hf = h5py.File(hdf5_path, 'r')
+        self.transforms = transforms
 
     def __getitem__(self, item):
-        hf=self.hf
-        image=hf['data']['image'][item]
+        hf = self.hf
+        transforms = self.transforms
+        image = hf['data']['image'][item]
         scales = hf['data']['scales'][item]
+        if isinstance(transforms, list):
+            for transform in transforms:
+                image = transform(image)
         return image, scales
+
     def __len__(self):
         return self.hf['data']['images'].len()
 
