f031372e2595d570bf077556c3aebc2d1948396d
Alexander <gogogox215@gmail.com>
2020-10-07 20:16:58 +0300

    more bugfixing

--- src/util/loss_functions.py
+++ src/util/loss_functions.py
@@ -9,25 +9,24 @@ def mse_weighted(weights, a, b):
     return torch.sqrt(torch.sum(loss))
 
 
-def vertice_mean_rms(mode_shapes, pow, scale_a: torch.tensor, scale_b: torch.tensor):
+def vertice_mean_rms(mode_shapes, pow, x: torch.tensor, y: torch.tensor):
     """
         return loss between movement (based on the scales) we received and the movement we calculated by our own scales
           Args:
               mode_shapes: a [V,n] tensor representing the mode shapes (only the Z axis)
               pow: the power of 10 used to scale the mode scales
-              scale_a: first set of scales
-              scale_b: second set of scales
+              x: first set of scales
+              y: second set of scales
            Returns:
               RMS between the vertex positions calculated from scales
            """
-    device=scale_a.device
-    scale_a=torch.Tensor(scale_a,device=device).detach()
-    scale_b=torch.Tensor(scale_b,device=device).detach()
-    scale_a *= 10 ** -pow
-    scale_b *= 10 ** -pow
-    scale_a=scale_a.to(torch.float64).T
-    scale_b=scale_b.to(torch.float64).T
-    mode_shapes = torch.tensor(mode_shapes, device=device, dtype=torch.float64).view(-1,len(scale_a))
-    pos_a = (mode_shapes@scale_a).sum(dim=1)
-    pos_b = (mode_shapes @ scale_b).sum(dim=1)
-    return torch.norm(pos_a - pos_b, 2)/pos_a.shape[0]
+    device = x.device
+    with torch.no_grad():
+        _x = x * 10 ** -pow
+        _y = y * 10 ** -pow
+        _x = _x.to(torch.float64).T
+        _y = _y.to(torch.float64).T
+        mode_shapes = torch.tensor(mode_shapes, device=device, dtype=torch.float64).view(-1, len(_x))
+        pos_a = (mode_shapes @ _x).sum(dim=1)
+        pos_b = (mode_shapes @ _y).sum(dim=1)
+        return torch.norm(pos_a - pos_b, 2) / pos_a.shape[0]
