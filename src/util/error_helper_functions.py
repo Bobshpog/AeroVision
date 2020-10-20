b60dbb6395fad36a8713375a430b1e2d35d8b6e8
Alexander <gogogox215@gmail.com>
2020-10-20 20:59:42 +0300

    implemented tensor updates to functions

--- src/util/error_helper_functions.py
+++ src/util/error_helper_functions.py
@@ -1,15 +1,18 @@
-
+from functools import partial
+import torch.nn.functional as F
 from src.util.loss_functions import *
 
-def calc_errors(loss_function, mode_shape, pow, ir_indices, x, y):
+
+def calc_errors(loss_function, mode_shapes, pow, ir_indices, device, x, y):
     """
     return errors as written in the exel file format
     Args:
-        loss_function: The loss function between two elements, should deal with vectors and single element.
+        loss_function: The loss function between two elements, should deal with vectors and single element,must work on tensors.
         ir_indices: tuple of ir indices
-        mode_shape: mode shape as read using matlab_reader.read_modal_shapes()
-        x: first set of scales (shape = data_points, num_of_scales)
-        y: second set of scales (shape = data_points, num_of_scales)
+        mode_shapes: mode shape as read using matlab_reader.read_modal_shapes()
+        x: first set of scales (shape = num_datapoints, num_of_scales)
+        y: second set of scales (shape = num_datapoints, num_of_scales)
+        device: device to run on, used only if x,y is an ndarray
 
     Returns:
         error values in the following error
@@ -17,23 +20,27 @@ def calc_errors(loss_function, mode_shape, pow, ir_indices, x, y):
         	Regression 0,	Regression 1,	Regression 2,	Regression 3,	Regression 4,	Regression 5,	Regression 6,
         		Regression 7,	Regression 8,	Regression 9)
     """
-    three_d_loss, ir_loss, avg_regression = 0, 0, 0
-    zero_scales = np.zeros(x.shape[1])
-    regression_loss = np.zeros((x.shape[1]))
-    data_points = x.shape[0]
-    num_of_scales = x.shape[1]
-    num_verticies = mode_shape.shape[1]
-    for i in trange(data_points):
-        three_d_loss += threeD_reconstruction_loss(loss_function, mode_shape, pow, x[i], y[i])
-        ir_loss += threeD_reconstruction_loss(loss_function, mode_shape[:,ir_indices], pow, x[i], y[i])
+    if isinstance(x, np.ndarray) or isinstance(y, np.ndarray):
+        _x = torch.tensor(x, device=device)
+        _y = torch.tensor(y, device=device)
+    else:
+        _x,_y=x,y
+    num_datapoints, num_of_scales = x.shape
+    device = _x.device
+    vertex_loss = reconstruction_loss_3d(loss_function, mode_shapes, pow, _x, _y)
+    ir_loss = reconstruction_loss_3d(loss_function, mode_shapes[:, ir_indices], pow, _x, _y)
+    regression_loss = torch.zeros(_x.shape[1], device=device)
+    for i in trange(num_datapoints, ):
         for k in range(num_of_scales):
-            regression_loss[k] += loss_function(zero_scales + x[i, k], zero_scales + y[i, k])
-        avg_regression += loss_function(x[i], y[i])
-    return (three_d_loss / (num_verticies * data_points), ir_loss / (len(ir_indices) * data_points),
-            avg_regression/(num_of_scales * data_points)) + tuple(regression_loss / data_points)
+            regression_loss[k] += loss_function(_x[i, k], _y[i, k])
+    regression_loss = regression_loss / num_datapoints
+    avg_regression = regression_loss.sum() / num_of_scales
+
+    return (vertex_loss, ir_loss / len(ir_indices),
+            avg_regression, regression_loss)
 
 
-def calc_max_errors(loss_function, scales: np.ndarray, ir_indices: tuple, mode_shape):
+def calc_max_errors(loss_function, scales: np.ndarray, ir_indices, mode_shape, device='cpu'):
     """
 
     Args:
@@ -41,6 +48,7 @@ def calc_max_errors(loss_function, scales: np.ndarray, ir_indices: tuple, mode_s
         scales: Scale ndarray in (n x num_scales)
         ir_indices: tuple of ir indices
         mode_shape: mode shape as read using matlab_reader.read_modal_shapes()
+        device: 'cpu' or 'cuda0'
 
     Returns:
         maximum error values in the following error
@@ -48,61 +56,70 @@ def calc_max_errors(loss_function, scales: np.ndarray, ir_indices: tuple, mode_s
         	Regression 0,	Regression 1,	Regression 2,	Regression 3,	Regression 4,	Regression 5,	Regression 6,
         		Regression 7,	Regression 8,	Regression 9,	)
     """
-    return (calc_max_3d_reconstruction_error(loss_function, scales, mode_shape),
-            calc_max_ir_reconstruction_error(loss_function, scales, ir_indices, mode_shape),
-            calc_max_regression_error(loss_function,scales)) + \
-            tuple(calc_max_per_param_error(loss_function,scales,range(scales.shape[1])))
+    _scales = torch.tensor(scales, device=device)
+    return (calc_max_3d_reconstruction_error(loss_function, _scales, mode_shape),
+            calc_max_ir_reconstruction_error(loss_function, _scales, ir_indices, mode_shape),
+            calc_max_regression_error(loss_function, _scales),
+            *calc_max_per_param_error(loss_function, _scales))
 
 
-def calc_max_3d_reconstruction_error(loss_function, scales, mode_shape):
-    max_3d = 0
-    ver = np.zeros(shape=(scales.shape[0], mode_shape.shape[1], mode_shape.shape[0]))
-    for i in range(scales.shape[0]):
-        #   creating deformation
-        ver[i] = (scales[i,:] * mode_shape).sum(axis=2).T
+def batch_mat_pdist(a: torch.Tensor, b: torch.Tensor, p=2):
+    """
+    Compute the pairwise distance_tensor matrix between a and b which both have size [n, d]. The result is a tensor of
+    size [n, n] whose entry [i, j] contains the distance_tensor between a[i, :] and b[j, :].
+    :param a: A tensor containing m batches of n points of dimension d. i.e. of size [n, d]
+    :param b: A tensor containing m batches of n points of dimension d. i.e. of size [n, d]
+    :param p: Norm to use for the distance_tensor
+    :return: A tensor containing the pairwise distance_tensor between each pair of inputs in a batch.
+    """
+    return (a.unsqueeze(1) - b.unsqueeze(0)).abs().pow(p).sum(2).pow(1 / p)
 
-    for i in trange(scales.shape[0]):
-        for j in range(scales.shape[0]):
-            curr = loss_function(ver[i], ver[j])
-            if curr > max_3d:
-                max_3d = curr
-    print("max 3d/ir "+f'{max_3d: .4e}')
-    return max_3d / mode_shape.shape[1]
+
+def calc_max_3d_reconstruction_error(loss_function, scales, mode_shapes, device='cpu'):
+    if loss_function in ('l1', 'l2'):
+        p = int(loss_function[1:])
+        num_scales = scales.shape[-1]
+        num_vertices = mode_shapes.size / (3 * num_scales)
+        with torch.no_grad():
+            _scales = scales.view(-1, num_scales).to(torch.float64).T
+            mode_shapes = torch.tensor(mode_shapes, device=device, dtype=torch.float64).reshape(-1, len(_scales))
+            point_clouds = (mode_shapes @ _scales).T
+            max_3d = batch_mat_pdist(point_clouds, point_clouds, p).max() / num_vertices
+    else:
+        raise NotImplementedError
+    print("max 3d/ir " + f'{max_3d[0]: .4e}')
+    return max_3d[0]
 
 
 def calc_max_ir_reconstruction_error(loss_function, scales, ir_indices, mode_shape):
-    return calc_max_3d_reconstruction_error(loss_function, scales, mode_shape[:,ir_indices])
+    return calc_max_3d_reconstruction_error(loss_function, scales, mode_shape[:, ir_indices])
 
 
-def calc_max_per_param_error(loss_function, scales, ids):
-    if isinstance(ids, int):
-        ids = [ids]
-    max_err = np.zeros(len(ids))
-    max_scale = np.zeros(scales.shape[0])
-    min_scale = np.zeros(scales.shape[0])
-    for i in range(scales.shape[0]):
-        max_scale[ids] = np.maximum(scales[i, ids], max_scale[ids])
-        min_scale[ids] = np.minimum(scales[i, ids], max_scale[ids])
-    for i in trange(scales.shape[0]):
-        for j in range(scales.shape[0]):
-            for num in ids:
-                curr = loss_function(scales[i, num], scales[j, num])
-                if curr > max_err[num]:
-                    max_err[num] = curr
+def calc_max_per_param_error(loss_function, scales: torch.Tensor):
+    max_scales = scales.max(dim=1)
+    min_scales = scales.min(dim=1)
+    max_err = []
+    if loss_function == 'l1':
+        loss_function = F.l1_loss
+    if loss_function == 'l2':
+        loss_function = lambda x, y: torch.norm(x - y)
+    for i, j in min_scales, max_scales:
+        max_err.append(loss_function(i, j))
+    max_err = tuple(max_err)
     print("modes error: " + max_err)
-    return max_err[ids]
+    return max_err
 
 
 def calc_max_regression_error(loss_function, scales):
-    max_scale, min_scale, max_error = 0, 0, 0
-    for i in trange(scales.shape[0]):
-        for j in range(scales.shape[0]):
-            curr = loss_function(scales[i,:], scales[j,:])
-            if curr > max_error:
-                max_error = curr
-    print("regression: "+f'{max_error/scales.shape[1]: .4e}')
-    return max_error/scales.shape[1]
-
+    num_scales=scales.shape[-1]
+    if loss_function in ('l1', 'l2'):
+        p = int(loss_function[1:])
+        with torch.no_grad():
+           max_error= batch_mat_pdist(scales, scales, p).max() /num_scales
+    else:
+        raise NotImplementedError
+    print("max 3d/ir " + f'{max_error[0]: .4e}')
+    return max_error[0]
 
 
 def error_to_exel_string(result):
@@ -111,4 +128,3 @@ def error_to_exel_string(result):
         exel_string += str(res) + " "
 
     return exel_string
-
