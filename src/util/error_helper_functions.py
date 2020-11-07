made reconstruction more numerically stable and fixed bug

src/util/error_helper_functions.py
@@ -21,8 +21,8 @@ def calc_errors(loss_function, mode_shapes: np.ndarray, pow, ir_indices, x, y):
     """
     num_datapoints, num_scales = x.shape
     # device = x.device
-    vertex_loss = reconstruction_loss_3d(loss_function, mode_shapes, pow, x * pow, y * pow)
-    ir_loss = reconstruction_loss_3d(loss_function, mode_shapes[:, ir_indices], pow, x * pow, y * pow)
+    vertex_loss = reconstruction_loss_3d(loss_function, mode_shapes, pow, x , y )
+    ir_loss = reconstruction_loss_3d(loss_function, mode_shapes[:, ir_indices], pow, x , y )
     regression_loss = torch.abs(x-y)
     avg_regression = regression_loss.sum(-1) / num_scales
     return (vertex_loss, ir_loss,

src/util/loss_functions.py
@@ -57,10 +57,10 @@ def reconstruction_loss_3d(loss_function, mode_shapes: np.ndarray, scale_factor:
     num_vertices = mode_shapes.size / (3 * x.shape[-1])
     device = x.device
     with torch.no_grad():
-        _x = x / scale_factor
-        _y = y / scale_factor
-        _x = _x.view(-1, _x.shape[-1]).to(torch.float64).T
-        _y = _y.view(-1, _y.shape[-1]).to(torch.float64).T
+        _x = torch.tensor(x, device=device, dtype=torch.float64).T / scale_factor
+        _y = torch.tensor(y, device=device, dtype=torch.float64).T / scale_factor
+        _x = _x.view(-1, x.shape[-1])
+        _y = _y.view(-1, y.shape[-1])
         mode_shapes = torch.tensor(mode_shapes, device=device, dtype=torch.float64).reshape(-1, len(_x))
         pos_a = (mode_shapes @ _x)
         pos_b = (mode_shapes @ _y)