f0ce4dd54a6173783e13700dc6440faf4532821a
Alexander <gogogox215@gmail.com>
2020-11-08 20:52:59 +0200

    fixed min_list

--- src/util/min_list.py
+++ src/util/min_list.py
@@ -18,10 +18,10 @@ class MinCounter:
         Returns:
             weather we didn't update the list for max_count epochs
         """
-        if x > self.data[-1]:
-            self.count += 1
-        else:
+        if x < self.data[0]:
             self.count = 0
             self.data[-1] = x
             self.data.sort()
+        else:
+            self.count += 1
         return self.count >= self.max_count
