8b2652d6d4ad5691b671227d206c2ddf714b2d48
Alexander <gogogox215@gmail.com>
2020-11-08 22:17:55 +0200

    ...

--- src/util/min_list.py
+++ src/util/min_list.py
@@ -8,7 +8,7 @@ class MinCounter:
     def __post_init__(self):
         if self.max_count < 0:
             raise ValueError
-        self.data = [(float("inf"), 0)]
+        self.data = [ -self.max_count+1,float("inf")]
         self.count = 0
 
     def add(self, epoch: int, x: float) -> bool:
@@ -18,10 +18,10 @@ class MinCounter:
         Returns:
             weather we didn't update the list for max_count epochs
         """
-        self.data = [x for x in self.data if ((epoch - x[1]) <= self.max_count)]
+        self.data = [x for x in self.data if ((epoch - x[0]) <= self.max_count)]
         if (not bool(self.data)):
             #if list is empty
             return True
-        self.data.append((x, epoch))
-        self.data.sort(key=lambda x: x[0])
+        self.data.append((epoch,x))
+        self.data.sort(key=lambda x: x[1])
         return False
