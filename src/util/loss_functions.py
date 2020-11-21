fixed l1 scaling

src/util/loss_functions.py
@@ -23,8 +23,8 @@ def l1_norm(y_hat, y):
     return (y_hat - y).abs()
 
 
-def l1_norm_indexed(i, y_hat, y):
-    return l1_norm(y_hat, y)[:,i]
+def l1_norm_indexed(output_scaling,i, y_hat, y):
+    return l1_norm(y_hat, y)[:,i]/output_scaling
 
 
 def vertex_mean_rms(mode_shapes, scale_factor, x: Union[torch.tensor, np.ndarray], y: Union[torch.tensor, np.ndarray]):