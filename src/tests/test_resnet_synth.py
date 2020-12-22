9f4e39b44e188015797600e8c28252c2a3c439c8
Alexander <gogogox215@gmail.com>
2020-12-22 10:38:12 +0200

    bugfix

--- src/tests/test_resnet_synth.py
+++ src/tests/test_resnet_synth.py
@@ -170,7 +170,7 @@ class TestCustomInputResnet(TestCase):
                                          OUTPUT_SCALE), 5, BATCH_SIZE)
                      }
         run_resnet_synth(NUM_INPUT_LAYERS, NUM_OUTPUTS, "test", TRAINING_DB_PATH, VALIDATION_DB_PATH, VAL_SPLIT,
-                         transform, reduce_dict, hist_dict, text_dict, train_cache_size=TRAIN_CACHE_SIZE,
+                         transform,None, reduce_dict, hist_dict, text_dict, train_cache_size=TRAIN_CACHE_SIZE,
                          val_cache_size=VAL_CACHE_SIZE,
                          batch_size=BATCH_SIZE, subsampler_size=len(VAL_SPLIT))
 
