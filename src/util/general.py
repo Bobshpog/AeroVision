3c0bda1aa518f44b8fcdb72f91b8db1e9aad5e9d
Alexander Portiankin <gogogox215@gmail.com>
2021-01-02 01:25:15 +0200

    fixed slowness of network due to bad imports

--- src/util/general.py
+++ src/util/general.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass
-import cv2
-from src.geometry.numpy.mesh import Mesh
-import numpy as np
+
 @dataclass
 class MinCounter:
     max_count: int
@@ -35,27 +33,27 @@ class Functor:
     def __call__(self, *args, **kwargs):
         return self.foo(*args,**kwargs)
 
-
-def create_stripes_texture(path, is_long_stripes, num_of_stripes=50, tex_resolution=(640,500,3), color1=(0,0,255),
-                           color2=(0,0,0),plot=False):     # colors in cv2 format (g,b,r)
-    num_pixel_per_stipe_long = int(tex_resolution[0]/num_of_stripes)
-    num_pixel_per_stipe_short = int(tex_resolution[1] / num_of_stripes)
-    zero = np.zeros(tex_resolution)
-    for i in range(num_of_stripes):
-        if i % 2 == 0:
-            if is_long_stripes:
-                zero[num_pixel_per_stipe_long*i:num_pixel_per_stipe_long*i+num_pixel_per_stipe_long,:, :] = color1
-            else:
-                zero[:, num_pixel_per_stipe_short * i:num_pixel_per_stipe_short * i + num_pixel_per_stipe_short, :] = color1
-        else:
-            if is_long_stripes:
-                zero[num_pixel_per_stipe_long*i:num_pixel_per_stipe_long*i+num_pixel_per_stipe_long,:, :] = color2
-            else:
-                zero[:, num_pixel_per_stipe_short * i:num_pixel_per_stipe_short * i + num_pixel_per_stipe_short, :] = color2
-
-    mesh = Mesh("data/wing_off_files/synth_wing_v5.off")
-    # mesh.plot_faces()
-    if plot:
-        cv2.imshow("frame", zero)
-        mesh.plot_faces(texture=path)
-    cv2.imwrite(path, zero)
+#
+# def create_stripes_texture(path, is_long_stripes, num_of_stripes=50, tex_resolution=(640,500,3), color1=(0,0,255),
+#                            color2=(0,0,0),plot=False):     # colors in cv2 format (g,b,r)
+#     num_pixel_per_stipe_long = int(tex_resolution[0]/num_of_stripes)
+#     num_pixel_per_stipe_short = int(tex_resolution[1] / num_of_stripes)
+#     zero = np.zeros(tex_resolution)
+#     for i in range(num_of_stripes):
+#         if i % 2 == 0:
+#             if is_long_stripes:
+#                 zero[num_pixel_per_stipe_long*i:num_pixel_per_stipe_long*i+num_pixel_per_stipe_long,:, :] = color1
+#             else:
+#                 zero[:, num_pixel_per_stipe_short * i:num_pixel_per_stipe_short * i + num_pixel_per_stipe_short, :] = color1
+#         else:
+#             if is_long_stripes:
+#                 zero[num_pixel_per_stipe_long*i:num_pixel_per_stipe_long*i+num_pixel_per_stipe_long,:, :] = color2
+#             else:
+#                 zero[:, num_pixel_per_stipe_short * i:num_pixel_per_stipe_short * i + num_pixel_per_stipe_short, :] = color2
+#
+#     mesh = Mesh("data/wing_off_files/synth_wing_v5.off")
+#     # mesh.plot_faces()
+#     if plot:
+#         cv2.imshow("frame", zero)
+#         mesh.plot_faces(texture=path)
+#     cv2.imwrite(path, zero)
